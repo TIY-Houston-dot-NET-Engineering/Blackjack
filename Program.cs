447fafcad01f684eab49bec767bf63ecf68aa575
Matt Keas <matthiasak@gmail.com>
2016-10-06 14:43:49 -0500

    creating initial preview

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace BlackJack
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Deck dealerDeck = new Deck();
+            // dealerDeck.cards; // [Card...]
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
