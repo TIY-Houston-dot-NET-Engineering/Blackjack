e82bed096e42d18343047f8afc0f8a77cd51dc27
Matt Keas <matthiasak@gmail.com>
2016-10-07 21:42:55 -0500

    adding full features:

--- Cards.cs
+++ Cards.cs
@@ -1,12 +1,13 @@
 using System;
 
 namespace BlackJack {
+
     public enum Suit
     {
         Hearts,
-        Clubs,
+        Spades,
         Diamonds,
-        Spades
+        Clubs
     }
 
     public enum Rank
@@ -30,6 +31,58 @@ namespace BlackJack {
         public Suit suit;
         public Rank rank;
 
+        public override string ToString(){
+            return $"{asSuit()} {asRank()}";
+        }
+
+        public string asSuit(){
+            switch(suit) {
+                case Suit.Hearts: return "♥";
+                case Suit.Clubs: return "♣";
+                case Suit.Diamonds: return "♦";
+                case Suit.Spades: return "♤";
+                default: return "___";
+            }
+        }
+
+        public string asRank() {
+            switch(rank){
+                case Rank.Ace: return "A";
+                case Rank.Deuce: return "2";
+                case Rank.Three: return "3";
+                case Rank.Four: return "4";
+                case Rank.Five: return "5";
+                case Rank.Six: return "6";
+                case Rank.Seven: return "7";
+                case Rank.Eight: return "8";
+                case Rank.Nine: return "9";
+                case Rank.Ten: return "10";
+                case Rank.Jack: return "J";
+                case Rank.Queen: return "Q";
+                case Rank.King: return "K";
+                default: return "__";
+            }
+        }
+
+        public int asScore(){
+            switch(rank){
+                case Rank.Ace: return 11;
+                case Rank.Deuce: return 2;
+                case Rank.Three: return 3;
+                case Rank.Four: return 4;
+                case Rank.Five: return 5;
+                case Rank.Six: return 6;
+                case Rank.Seven: return 7;
+                case Rank.Eight: return 8;
+                case Rank.Nine: return 9;
+                case Rank.Ten: return 10;
+                case Rank.Jack: return 10;
+                case Rank.Queen: return 10;
+                case Rank.King: return 10;
+                default: return 0;
+            }
+        }
+
         public Card(Suit s, Rank r){
             suit = s;
             rank = r;
@@ -37,7 +90,10 @@ namespace BlackJack {
     }
 
     public class Deck {
+
         public Card[] cards;
+        private int numDealt = 0;
+
         public Deck(){
             cards = new Card[52];
             int i = 0; // 1
@@ -51,9 +107,76 @@ namespace BlackJack {
                 }
                 i++;
             }
+
+            randomize(); // shuffle card array
         }
-        public Card[] getRandomized(){
-            return new Card[0];
+
+        //  swap two indices
+        public static void swap(Card[] arr, int a, int b){
+            var temp = arr[a];
+            arr[a] = arr[b];
+            arr[b] = temp;
+        }
+
+        public void randomize(){
+            for(int i = 0; i < 52; i++){
+                int r = new Random().Next(i, 51);
+                swap(cards, i, r);
+            }
+        }
+
+        public Card deal() => 
+            (numDealt < cards.Length) ? cards[numDealt++] : null;
+    }
+
+    public class Hand {
+        public Card[] cards = new Card[0];
+        
+        public int score(){
+            int s = 0;
+            foreach(Card c in cards){
+                s += c.asScore();
+            }
+            return s;
+        }
+
+        public override string ToString(){
+            string r = "";
+            int i = 0;
+            foreach(Card c in cards) {
+                r = i++ == 0 ? $"{c}" : $"{r}, {c}";
+            }
+            return $"Player Hand: {r} - score: {score()}";
+        }
+
+        public void take(Card newCard) {
+            var temp = new Card[cards.Length + 1];
+            int i = 0;
+            foreach(Card c in cards){
+                temp[i++] = c;
+            }
+            temp[i] = newCard;
+            cards = temp;
+        }
+    }
+
+    public class DealerHand : Hand {
+        public override string ToString(){
+            string r = "";
+            int i = 0;
+            foreach(Card c in cards) {
+                r = i++ == 0 ? $"{c}" : $"{r}, *";
+            }
+            return $"Dealer Hand: {r} - score: ???";
+        }
+
+        public string show(){
+            string r = "";
+            int i = 0;
+            foreach(Card c in cards) {
+                r = i++ == 0 ? $"{c}" : $"{r}, {c}";
+            }
+            return $"Dealer Hand: {r} - score: {score()}";
         }
     }
 }
--- Program.cs
+++ Program.cs
@@ -6,9 +6,75 @@ namespace BlackJack
     {
         public static void Main(string[] args)
         {
-            Deck dealerDeck = new Deck();
-            // dealerDeck.cards; // [Card...]
-            Console.WriteLine("Hello World!");
+            // Deck myDeck = new Deck();
+            // Hand player = new Hand();
+            // player.take(myDeck.deal());
+            // Console.WriteLine(player);
+            NewGame();
         }
+
+        public static void NewGame(){
+            Deck randomized = new Deck();
+            Hand player = new Hand();
+            DealerHand dealer = new DealerHand();
+
+            Action<bool> print = (showDealer) => {
+                Console.WriteLine(player);
+                Console.WriteLine(showDealer ? dealer.show() : dealer.ToString());
+            };
+
+            Console.WriteLine("=======");
+
+            // two initial deals
+            player.take(randomized.deal());
+            player.take(randomized.deal());
+            dealer.take(randomized.deal());
+            dealer.take(randomized.deal());
+
+            while(dealer.score() < 16){
+                dealer.take(randomized.deal());
+            }
+
+            if(player.score() < 21){
+                print(false);
+
+                // while "hit me"
+                while(player.score() < 21 && prompt()){
+                    player.take(randomized.deal());
+
+                    print(false);
+
+                    // game ends if user hits 21 or over 21
+                    if(player.score() >= 21){
+                        break;
+                    }
+                }
+            }
+
+            int a = player.score();
+            int b = dealer.score();
+
+            print(true);
+            
+            if(b <= 21  && (a > 21 || a < b)) {
+                Console.WriteLine("\n\n -------- Dealer wins! -------- \n\n");
+            } else if(b > 21 && a > 21 || a == b){
+                Console.WriteLine("\n\n -------- Draw. -------- \n\n");
+            } else {
+                Console.WriteLine("\n\n -------- You win! -------- \n\n");
+            }
+
+            // play again?
+            Console.WriteLine("++++++Play again? (y/N)++++++");
+            if(Console.ReadKey().KeyChar.ToString().ToUpper() == "Y")
+                NewGame();
+        }
+
+        public static bool prompt(){
+            Console.WriteLine("\n\nHit me? (y/N)\n\n");
+            string result = Console.ReadKey().KeyChar.ToString().ToUpper();
+            Console.WriteLine("\n\n\n\n\n ");
+            return result == "Y";
+        } 
     }
 }
